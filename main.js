6526802b8f754f620f617c246810eb51b7c89471
vihaan1245 <96100376+vihaan1245@users.noreply.github.com>
2022-02-11 09:16:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/s6feeOL0o/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+  }
+}
